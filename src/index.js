4b0df7ba2157a42efc5d496bc9edbf2e785bf3e4
Sergey <segaoff@icloud.com>
2022-03-19 23:18:53 +0300

    создал js файл для правлного скролла картинки 'up'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import menu from './modules/menu'
 import modal from './modules/modal'
+import serviceScroll from './modules/serviceScroll'
 
 modal()
 menu()
+serviceScroll()
---
+++ src/modules/serviceScroll.js
@@ -0,0 +1,7 @@
+import { smoothScroll } from "./helper"
+
+const serviceScroll = () => {
+    console.log('123')
+}
+
+export default serviceScroll
